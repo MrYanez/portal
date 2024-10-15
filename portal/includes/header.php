71cdd2ce303af6bae7c922d2ccc67b141bbf35fe
MrYanez <129912196+MrYanez@users.noreply.github.com>
2024-10-15 09:47:03 -0700

    Add files via upload

---
+++ portal/includes/header.php
@@ -0,0 +1,35 @@
+
+<!--header.php-->
+<DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta http-equiv='cache-control' content='no-cache'> 
+            <meta http-equiv='expires' content='0'> 
+            <meta http-equiv='pragma' content='no-cache'>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>header.php</title>
+            <link href="./css/index_styles.css" type="text/css" rel="stylesheet">
+        </head>
+
+        <body class="home">   
+            <div id="wrapper">  
+        <header>
+             <section class="tittle" alt="title">
+            <h1>My Portal Page</h1>
+            </section>
+                <nav id="primary">
+                    <ul>
+                        <li><a href="index.html">INDEX</a></li>
+                        <li><a href="about.html">ABOUT</a></li>
+                        <li><a href="Work.html">WORK</a></li>
+                        <li><a href="gallery.html">GALLERY</a></li>
+                        <li><a href="contact.html">CONTACT</a></li>
+                    </ul>
+                </nav>
+                </header>
+
+            
+                <!--header ends here-->
+         
